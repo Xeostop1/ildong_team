9325b0d6b42cae9afbd10089d49849bcf809e371
Xeostop1 <stop1@daum.net>
2023-07-26 17:14:59 +0900

    동국 마무리

--- pre_foler/txt_search.py
+++ pre_foler/txt_search.py
@@ -21,7 +21,7 @@ def find_cells_with_two_words(input_file, output_file, word1, word2):
 
 
 # 사용 예
-word1 = "한의원"
+word1 = "뒤집"
 word2 = "기미"  
 input_file = "./pre_foler/gimi_origin.xlsx"  #원본 엑셀 파일 경로
 output_file = f"{word1}_{word2}.txt"  # 출력 TXT 파일 경로
